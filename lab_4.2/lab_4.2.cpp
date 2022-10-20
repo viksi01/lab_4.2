864174fc4a0167b1489adc573259e03b94e496d3
viksi01 <victoria.pyatnicka@gmail.com>
2022-10-20 23:39:50 +0300

    commit1_master

---
+++ lab_4.2/lab_4.2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << "     |"
+		 << setw(7) << "y" << "       |" << endl;
+	cout << "---------------------------" << endl;
+
+	x = xp;
+	while (x <= xk)
+	{
+		A = 2 * x - 13.5; 
+		if (x < -1)
+			B = sin(x) / (1 + cos(x) * cos(x));
+		else
+			if (x > 1)
+				B = log10(x + 0.4);
+			else
+				B = cos(sin(x) * sin(x)) * cos(sin(x) * sin(x)) - 1;
+		y = A - B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			 << "   |" << setw(10) << setprecision(3) << y
+			 << "    |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
